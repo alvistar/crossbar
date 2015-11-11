9f0a0b855524ad1aa689fddc9a8c389934e3f826
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-11-11 23:09:10 +0100

    refactoring

--- crossbar/router/broker.py
+++ crossbar/router/broker.py
@@ -30,8 +30,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-from collections import deque
-
 from autobahn import util
 from autobahn.wamp import role
 from autobahn.wamp import message
@@ -45,111 +43,11 @@ from autobahn.wamp.message import \
 from crossbar.router.observation import UriObservationMap
 from crossbar.router import RouterOptions, RouterAction
 from crossbar._logging import make_logger
+from crossbar.router.realmstore import MemoryEventStore
 
 import txaio
 
 __all__ = ('Broker',)
-
-
-class EventStore(object):
-    """
-    Event store in-memory implementation.
-    """
-
-    log = make_logger()
-
-    def __init__(self):
-        # part of event history: map of publication_id -> event dict
-        self._event_store = {}
-
-        # part of event history: map of subscription_id -> list of publication_id
-        self._event_history = {}
-
-    def store_event(self, publisher_id, publication_id, topic, args=None, kwargs=None):
-        """
-        Persist the given event to history.
-
-        :param publisher_id: The session ID of the publisher of the event being persisted.
-        :type publisher_id: int
-        :param publication_id: The publication ID of the event.
-        :type publisher_id: int
-        :param topic: The topic URI of the event.
-        :type topic: unicode
-        :param args: The args payload of the event.
-        :type args: list or None
-        :param kwargs: The kwargs payload of the event.
-        :type kwargs: dict or None
-        """
-        assert(publication_id not in self._event_store)
-        evt = {
-            'publisher': publisher_id,
-            'publication': publication_id,
-            'topic': topic,
-            'args': args,
-            'kwargs': kwargs
-        }
-        self._event_store[publication_id] = evt
-        self.log.debug("event {publication_id} persisted", publication_id=publication_id)
-
-    def store_event_history(self, publication_id, subscription_id):
-        """
-        Persist the given publication history to subscriptions.
-
-        :param publication_id: The ID of the event publication to be persisted.
-        :type publication_id: int
-        :param subscription_id: The ID of the subscription the event (identified by the publication ID),
-            was published to, because the event's topic matched the subscription.
-        :type subscription_id: int
-        """
-        assert(publication_id in self._event_store)
-        if subscription_id not in self._event_history:
-            self._event_history[subscription_id] = deque()
-        self._event_history[subscription_id].append(publication_id)
-        self.log.debug("event {publication_id} history persisted for subscription {subscription_id}", publication_id=publication_id, subscription_id=subscription_id)
-
-    def get_events(self, subscription_id, limit):
-        """
-        Retrieve given number of last events for a given subscription.
-
-        If no history is maintained for the given subscription, None is returned.
-
-        :param subscription_id: The ID of the subscription to retrieve events for.
-        :type subscription_id: int
-        :param limit: Limit number of events returned.
-        :type limit: int
-
-        :return: List of events.
-        :rtype: list or None
-        """
-        if subscription_id not in self._event_history:
-            return None
-        else:
-            s = self._event_history[subscription_id]
-
-            # at most "limit" events in reverse chronological order
-            res = []
-            i = -1
-            if limit > len(s):
-                limit = len(s)
-            for _ in range(limit):
-                res.append(self._event_store[s[i]])
-                i -= 1
-            return res
-
-    def get_event_history(self, subscription_id, from_ts, until_ts):
-        """
-        Retrieve event history for time range for a given subscription.
-
-        If no history is maintained for the given subscription, None is returned.
-
-        :param subscription_id: The ID of the subscription to retrieve events for.
-        :type subscription_id: int
-        :param from_ts: Filter events from this date (string in ISO-8601 format).
-        :type from_ts: unicode
-        :param until_ts: Filter events until this date (string in ISO-8601 format).
-        :type until_ts: unicode
-        """
-        raise Exception("not implemented")
 
 
 class Broker(object):
@@ -189,7 +87,7 @@ class Broker(object):
                                                       subscription_revocation=True)
 
         # store for event history
-        self._event_store = EventStore()
+        self._event_store = MemoryEventStore()
 
         if self._event_store:
             # example topic being configured as persistent
